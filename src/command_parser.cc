9d1155f7cbb7050ff9e04f900584c76fdaa57080
Joris Vankerschaver <joris.vankerschaver@gmail.com>
2021-12-14 14:06:54 +0900

    MAINT: Factor out parameters for subcommands

--- src/command_parser.cc
+++ src/command_parser.cc
@@ -20,8 +20,46 @@ IndexAlgorithm stringToIndexAlgorithm(const std::string &algorithm)
     }
     else
     {
-        throw ExitError("invalid algorithm", -1); // TODO make nice
+        std::stringstream ss;
+        ss << "Invalid algorithm: " << algorithm;
+        throw ExitError(ss.str(), -1);
     }
+}
+
+void AddToplevelArguments(CLI::App &app, Parameters &params)
+{
+    app.add_option_function<std::string>(
+        "--algorithm",
+        [&params](const std::string &algorithm)
+        {
+            params.index_algorithm = stringToIndexAlgorithm(algorithm);
+        },
+        "algorithm to use for searching/indexing.");
+}
+
+CLI::App *AddBuildSubCommand(CLI::App &app, Parameters &params)
+{
+    CLI::App *build_sc = app.add_subcommand("build", "build search index");
+
+    build_sc
+        ->add_option("sequences", params.sequences_path, "FASTA file to index")
+        ->required();
+
+    return build_sc;
+}
+
+CLI::App *AddQuerySubCommand(CLI::App &app, Parameters &params)
+{
+    CLI::App *query_sc = app.add_subcommand("query", "query previously-built search index");
+
+    query_sc
+        ->add_option("database", params.database_path, "Database to query")
+        ->required();
+    query_sc
+        ->add_option("query", params.query_path, "Query file in FASTA format")
+        ->required();
+
+    return query_sc;
 }
 
 Parameters parse_command_line_args(int argc, char **argv)
@@ -29,20 +67,11 @@ Parameters parse_command_line_args(int argc, char **argv)
     CLI::App app{"Sequence indexing and querying using NMSLIB"};
     app.require_subcommand(1);
 
-    std::string algorithm;
-    app.add_option("--algorithm", algorithm, "algorithm to use for searching/indexing.");
+    Parameters p;
 
-    CLI::App *build_sc = app.add_subcommand("build", "build search index");
-
-    fs::path sequences_path;
-    build_sc->add_option("sequences", sequences_path, "FASTA file to index")->required();
-
-    CLI::App *query_sc = app.add_subcommand("query", "query previously-built search index");
-
-    fs::path database_path;
-    query_sc->add_option("database", database_path, "Database to query")->required();
-    fs::path query_path;
-    query_sc->add_option("query", query_path, "Query file in FASTA format")->required();
+    AddToplevelArguments(app, p);
+    auto build_sc = AddBuildSubCommand(app, p);
+    auto query_sc = AddQuerySubCommand(app, p);
 
     try
     {
@@ -60,20 +89,13 @@ Parameters parse_command_line_args(int argc, char **argv)
         throw ExitError("An unknown error occurred while parsing command-line options.", -1);
     }
 
-    Parameters p;
-
     if (build_sc->parsed())
     {
         p.mode = RunMode::build;
-        p.index_algorithm = stringToIndexAlgorithm(algorithm);
-        p.sequences_path = sequences_path;
     }
     else if (query_sc->parsed())
     {
         p.mode = RunMode::query;
-        p.index_algorithm = stringToIndexAlgorithm(algorithm);
-        p.database_path = database_path;
-        p.query_path = query_path;
     }
 
     return p;

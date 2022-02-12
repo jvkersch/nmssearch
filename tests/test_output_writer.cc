2a224397d627e2575a7f1cbd48579bb2d320c878
Joris Vankerschaver <joris.vankerschaver@gmail.com>
2022-02-12 21:22:49 +0900

    TST: Tests for OutputWriter

--- tests/test_output_writer.cc
+++ tests/test_output_writer.cc
@@ -1,12 +1,55 @@
+#include <sstream>
+
 #include <catch2/catch_test_macros.hpp>
 
-unsigned int Factorial( unsigned int number ) {
-    return number <= 1 ? number : Factorial(number-1)*number;
-}
+#include "output_writer.h"
 
-TEST_CASE( "Factorials are computed", "[factorial]" ) {
-    REQUIRE( Factorial(1) == 1 );
-    REQUIRE( Factorial(2) == 2 );
-    REQUIRE( Factorial(3) == 6 );
-    REQUIRE( Factorial(10) == 3628800 );
+
+TEST_CASE( "AlignStats are displayed correctly", "[output]" ) {
+
+    AlignStats a = {
+        "query1",
+        "subject1",
+        95.8,
+        200,
+        20,
+        10,
+        1,
+        201,
+        2,
+        202,
+        1e-5,
+        1024
+    };
+    AlignStats b = {
+        "query2",
+        "subject2",
+        99.9,
+        200,
+        20,
+        10,
+        1,
+        201,
+        2,
+        202,
+        1e-5,
+        1024
+    };
+
+    OutputWriter writer;
+
+    std::string expected1 = "query1\tsubject1\t95.80\t200\t20\t10\t1\t201\t2\t202\t0.00\t1024\n";
+    std::string expected2 = "query2\tsubject2\t99.90\t200\t20\t10\t1\t201\t2\t202\t0.00\t1024\n";
+
+    SECTION("Single records are displayed correctly") {
+        std::stringstream ss;
+        writer.display(a, ss);
+        REQUIRE(ss.str() == expected1);
+    }
+
+    SECTION("Multiple records are displayed correctly") {
+        std::stringstream ss;
+        writer.display({a, b}, ss);
+        REQUIRE(ss.str() == expected1 + expected2);
+    }
 }

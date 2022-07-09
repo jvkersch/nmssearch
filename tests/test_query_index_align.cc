b81135b1539295bc5c3554cdc6a8329b2e093892
Joris Vankerschaver <joris.vankerschaver@gmail.com>
2022-07-09 11:08:55 +0200

    TST: vptree-align test

--- tests/test_query_index_align.cc
+++ tests/test_query_index_align.cc
@@ -36,26 +36,12 @@ private:
 
 TEST_CASE("Query the index in align mode", "[query-align]") {
 
-    // Write sequence database
-    std::stringstream s;
-    s << ">A\n"
-       << "AAATTGG\n"
-       << ">B\n"
-       << "AAAGGT\n";
-    auto database = CreateTempFile(s.str());
-
-    // Write query file
-    std::stringstream t;
-    t << ">q\n"
-      << "AAAACGT\n";
-    auto query = CreateTempFile(t.str());
-
     SECTION("bruteforce") {
         // Given
         Parameters params;
         params.index_algorithm = IndexAlgorithm::bruteforce;
-        params.database_path = database;
-        params.query_path = query;
+        params.database_path = testing_artifact("seqs-small.fa");
+        params.query_path = testing_artifact("query-small.fa");
         DummyOutputWriter writer;
 
         AlignQueryIndexCommand cmd(params, writer);
@@ -71,6 +57,24 @@ TEST_CASE("Query the index in align mode", "[query-align]") {
         REQUIRE(writer[1].pctIdentity == Catch::Approx(83.33).margin(0.01));
     }
 
-    RemoveTempFile(query);
-    RemoveTempFile(database);
+    SECTION("vptree") {
+        // Given
+        Parameters params;
+        params.index_algorithm = IndexAlgorithm::vptree;
+        params.database_path = testing_artifact("vptree-small.bin");
+        params.query_path = testing_artifact("query-small.fa");
+        DummyOutputWriter writer;
+
+        AlignQueryIndexCommand cmd(params, writer);
+
+        // When
+        cmd.run();
+
+        // Then
+        REQUIRE(writer.ncalls() == 2);
+        REQUIRE(writer[0].subjectId == "A");
+        REQUIRE(writer[0].pctIdentity == Catch::Approx(57.14).margin(0.01));
+        REQUIRE(writer[1].subjectId == "B");
+        REQUIRE(writer[1].pctIdentity == Catch::Approx(83.33).margin(0.01));
+    }
 }
--- tests/testing_helpers.h
+++ tests/testing_helpers.h
@@ -3,8 +3,12 @@
 
 #include <cstdio>
 #include <cstdlib>
+#include <filesystem>
 #include <iostream>
 #include <string>
+#include <sstream>
+
+namespace fs = std::filesystem;
 
 
 // Create a temporary file with the given contents. Caller needs to unlink the file.
@@ -26,6 +30,16 @@ inline std::string CreateTempFile(const std::string &contents) {
 // Unlink the given temporary file.
 inline void RemoveTempFile(const std::string &filename) {
     std::remove(filename.c_str());
+}
+
+inline fs::path testing_artifact(std::string name) {
+    fs::path full_path = fs::path(TEST_ARTIFACT_DIRECTORY) / name;
+    if (!fs::exists(full_path)) {
+        std::stringstream ss;
+        ss << "File not found: " << full_path;
+        throw std::runtime_error(ss.str());
+    }
+    return full_path;
 }
 
 #endif // _TESTING_HELPERS_H_

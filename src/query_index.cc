ee38cf28b8a70738b3e8b8845146ebac4f083aac
Joris Vankerschaver <joris.vankerschaver@gmail.com>
2022-07-11 19:28:05 +0200

    ENH: Set threading for bruteforce mode

--- src/query_index.cc
+++ src/query_index.cc
@@ -5,9 +5,12 @@ similarity::AnyParams QueryIndexCommand::getQueryParameters() const {
 
     similarity::AnyParams queryParams;
 
-    if (m_params.index_algorithm == IndexAlgorithm::vptree) {
+    auto algo = m_params.index_algorithm;
+    if (algo == IndexAlgorithm::vptree) {
         queryParams.AddChangeParam("alphaLeft", 1.0);
         queryParams.AddChangeParam("alphaRight", 1.0);
+    } else if ((algo == IndexAlgorithm::bruteforce) || (algo == IndexAlgorithm::bruteforce_kmer)) {
+        queryParams.AddChangeParam("threadQty", m_params.indexThreadQty);
     }
     
     return queryParams;

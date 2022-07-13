8133ac5f1272442f1e460956dcad100e8875d6c6
Joris Vankerschaver <joris.vankerschaver@gmail.com>
2022-07-13 14:16:03 +0200

    FIX: Bruteforce kmer mode

--- src/build_index_spectrum.cc
+++ src/build_index_spectrum.cc
@@ -31,22 +31,10 @@ inline similarity::ObjectVector CreateDataset(size_t n) {
 namespace fs = std::filesystem;
 
 void SpectrumBuildIndexCommand::run() const {
-  // NeedlemanWunschAligner aligner;
-  // AlignmentSpace<NeedlemanWunschAligner> nwspace(aligner);
 
   std::cerr << "Building index for " << m_params.sequences_path
             << " using algorithm "
             << IndexAlgorithmToString(m_params.index_algorithm) << std::endl;
-
-  // Early exit for brute-force, which doesn't build an index
-  if (m_params.index_algorithm == IndexAlgorithm::bruteforce_kmer) {
-    std::cerr << "Brute-force algorithm does not require an index.";
-    return;
-  }
-
-  // // Read sequences
-  // FASTASequenceReader reader(m_params.sequences_path);
-  // SequenceContainer database(reader);
 
   auto database =
       LoadSpectraFromFile(m_params.sequences_path, m_params.kmer_length);
@@ -59,42 +47,34 @@ void SpectrumBuildIndexCommand::run() const {
   int seed = 1234;
   similarity::initLibrary(seed, LIB_LOGNONE, NULL);
 
-  // Create index
+  // Persist spectra
+  auto db = m_params.database_path;
+  fs::remove_all(db);
+  fs::create_directories(db);
+  fs::copy(m_params.sequences_path, db / "sequences.fa");
+  serialize(database, db / "spectra.bin");
+
+ // Create index
   similarity::AnyParams indexParams;
   std::string method_name;
-
   indexParams.AddChangeParam("indexThreadQty", m_params.indexThreadQty);
 
   if (m_params.index_algorithm == IndexAlgorithm::hnsw_kmer) {
     method_name = "hnsw";
     indexParams.AddChangeParam("M", m_params.M);
     indexParams.AddChangeParam("efConstruction", m_params.efConstruction);
+  
+    std::cerr << "Parameters: " << indexParams.ToString() << std::endl;
+
+    auto index = std::unique_ptr<similarity::Index<float>>(
+        similarity::MethodFactoryRegistry<float>::Instance().CreateMethod(
+            true, method_name, "custom", space, indices));
+
+    index->CreateIndex(indexParams);
+    std::cout << "INDEX SIZE: " << index->GetSize() << std::endl;
+
+    index->SaveIndex(db / "index.bin");
   }
-  // } else if (m_params.index_algorithm == IndexAlgorithm::vptree) {
-  //   method_name = "vptree";
-  //   indexParams =
-  //       similarity::AnyParams({"bucketSize=1", "selectPivotAttempts=1"});
-  // }
-  std::cerr << "Parameters: " << indexParams.ToString() << std::endl;
-
-  auto index = std::unique_ptr<similarity::Index<float>>(
-      similarity::MethodFactoryRegistry<float>::Instance().CreateMethod(
-          true, method_name, "custom", space, indices));
-
-  index->CreateIndex(indexParams);
-  std::cout << "INDEX SIZE: " << index->GetSize() << std::endl;
-
-  // Persist the index
-  auto db = m_params.database_path;
-  fs::remove_all(db);
-  fs::create_directories(db);
-  fs::copy(m_params.sequences_path, db / "sequences.fa");
-  index->SaveIndex(db / "index.bin");
-  serialize(database, db / "spectra.bin");
 
   std::cerr << "Saved index to " << db << std::endl;
-
-  // if (m_params.instrumentation) {
-  //     std::cerr << "Calls to aligner: " << aligner.ncalls() << std::endl;
-  // }
 }
--- tests/test_build_index_spectrum.cc
+++ tests/test_build_index_spectrum.cc
@@ -31,21 +31,21 @@ TEST_CASE("can build an index (spectrum mode)", "[build-index]") {
         fs::remove_all(params.database_path);
     }
 
-    // SECTION("vptree") {
+    SECTION("bruteforce (spectrum)") {
 
-    //     // Given
-    //     Parameters params;
-    //     params.index_algorithm = IndexAlgorithm::vptree;
-    //     params.sequences_path = testing_artifact("seqs-small.fa");
-    //     params.database_path = "seqs-small-tmp.bin";
-    //     AlignBuildIndexCommand command(params);
+        // Given
+        Parameters params;
+        params.index_algorithm = IndexAlgorithm::bruteforce_kmer;
+        params.sequences_path = testing_artifact("seqs-small.fa");
+        params.database_path = "seqs-small-kmer-tmp.bin";
+        SpectrumBuildIndexCommand command(params);
 
-    //     // When
-    //     command.run();
+        // When
+        command.run();
 
-    //     // Then
-    //     require_exists(params.database_path / "index.bin");
-    //     require_exists(params.database_path / "sequences.fa");
-    //     fs::remove_all(params.database_path);
-    // }
+        // Then
+        require_exists(params.database_path / "spectra.bin");
+        require_exists(params.database_path / "sequences.fa");
+        fs::remove_all(params.database_path);
+    }
 }
